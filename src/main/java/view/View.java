bd1e4e30fea376404adc13dd4a5004cba6e769bb
poonnatuch B <poonnatuch@siam-u.ac.th>
2021-04-12 03:29:13 +0700

    Make header dynamic to username

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -5,15 +5,28 @@ package view;
  */
 public class View {
 
+    private String line = "===========================================";
+
     public void head(String username) {
-        System.out.println("===========================================");
-         System.out.printf("= %s ==================================\n", username);
-        System.out.println("===========================================");
+
+        System.out.println(line);
+
+        System.out.printf("== " + "%s",username + " =");
+        for (int i = 0; i < line.length() - (username.length()+5); i++) {
+            System.out.print("=");
+        }
+        System.out.println();
+
+        System.out.println(line);
     }
 
     public void head() {
-        System.out.println("===========================================");
+        System.out.println(line);
         System.out.println("= Welcome =================================");
-        System.out.println("===========================================");
+        System.out.println(line);
+    }
+
+    public void foot() {
+        System.out.println(line);
     }
 }
