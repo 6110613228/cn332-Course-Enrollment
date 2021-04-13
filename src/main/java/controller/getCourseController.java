a9f40c322962b3c254167afe4fb31014e0ce267e
poonnatuch B <poonnatuch@siam-u.ac.th>
2021-04-14 03:12:27 +0700

    Create class template

---
+++ src/main/java/controller/getCourseController.java
@@ -0,0 +1,25 @@
+package controller;
+
+/**
+ * getCourseController
+ */
+class getCourseController implements Controller {
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void execute(Object a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void execute(Object a, Object b) {
+        // TODO Auto-generated method stub
+        
+    }
+}
---
+++ src/main/java/controller/showEnrollmentRatingController.java
@@ -0,0 +1,25 @@
+package controller;
+
+/**
+ * showEnrollmentRatingController
+ */
+class showEnrollmentRatingController implements Controller {
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void execute(Object a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void execute(Object a, Object b) {
+        // TODO Auto-generated method stub
+        
+    }
+}
