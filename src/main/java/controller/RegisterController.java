9baa8e9f2b5fc929322be812d283a81f0cef3c4c
poonnatuch B <poonnatuch@siam-u.ac.th>
2021-04-14 18:24:35 +0700

    Update RegisterController

---
+++ src/main/java/controller/RegisterController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import model.UserModel;
+
+/**
+ * RegisterController
+ */
+class RegisterController implements Controller {
+
+    UserModel model = new UserModel();
+
+    String name;
+    String surname;
+    String password;
+    String address;
+    String phone;
+    String email;
+
+    public void set(String name, String surname, String password, String address, String phone, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public void execute() {
+        model.addUser(name, surname, password, address, phone, email);
+    }
+    @Override
+    public void execute(Object a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void execute(Object a, Object b) {
+        // TODO Auto-generated method stub
+        
+    }
+}
